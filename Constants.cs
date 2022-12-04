50d10979216e3ffcbf60678e17244b820106a7b0
rayen <rayen.andolsi98@gmail.com>
2022-12-04 20:44:32 +0100

    First commit to cloned repo

---
+++ Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tp4
+{
+    public static class Constants
+    {
+        public const string OpenWeatherMapEndpoint =
+       "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey =
+       "dbc5f990e7f9b84f2b6e5ac79eb8a9b7";
+    }
+}
+
